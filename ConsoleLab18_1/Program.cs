4d8dcad3d5b4c1100209d8bba2353838851907bd
Роман Роман <romanbindyug@yandex.ru>
2022-01-19 21:28:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleLab18_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLab18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = @"([]{})[]";
+            char[] strArray = str.ToCharArray();
+            Stack<char> stack = new Stack<char>();
+            foreach (char item in strArray)
+            {
+                try
+                {
+                    if (item == ')' && stack.Peek() == ')')
+                    {
+                        stack.Pop();
+                    }
+                    if (item == '}' && stack.Peek() == '}')
+                    {
+                        stack.Pop();
+                    }
+                    if (item == ']' && stack.Peek() == ']')
+                    {
+                        stack.Pop();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Скобки расствлены НЕ корректно! Стек пуст");
+                    Console.WriteLine();
+                    Console.WriteLine("Для завершения нажмите любую клавишу");
+                    Console.ReadKey();
+                    return;
+                }
+                
+                if (item=='(')
+                {
+                    stack.Push(')');
+                }
+                if (item == '{')
+                {
+                    stack.Push('}');
+                }
+                if (item == '[')
+                {
+                    stack.Push(']');
+                }
+            }
+            Console.WriteLine(strArray);
+            Console.WriteLine();
+            if (stack.Count()==0)
+            {
+                Console.WriteLine("Скобки расствлены корректно");
+            }
+            else
+            {
+                Console.WriteLine("Скобки расствлены НЕ корректно");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
